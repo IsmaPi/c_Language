5478cf08a9f95f56075719850053b1e26dc06394
jorgevart <124254713+jorgevart@users.noreply.github.com>
2023-12-17 20:24:18 +0100

    Update visitor.h

--- C_Final_Project/src/include/visitor.h
+++ C_Final_Project/src/include/visitor.h
@@ -1,11 +1,19 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 #include "AST.h"
+#include "scope.h"  // Assuming you use scopes for variable management
 
 // Visitor structure for traversing and interpreting the AST
 typedef struct VISITOR_STRUCT {
-    // You can add fields here if you need to maintain state
-    // within the visitor (e.g., for storing results, managing environment, etc.)
+    // Current scope: Useful for tracking variables and their values
+    scope_T* current_scope;
+
+    // Error handling fields
+    int error_flag;  // A flag to indicate if an error has occurred
+    char* error_message;  // A message describing the error
+
+    int recursion_depth;  // To track recursion depth if needed
+
 } visitor_T;
 
 // Function prototypes for the visitor
@@ -20,10 +28,9 @@ AST_T* visitor_visit_function_call(visitor_T* visitor, AST_T* node);
 AST_T* visitor_visit_string(visitor_T* visitor, AST_T* node);
 AST_T* visitor_visit_compound(visitor_T* visitor, AST_T* node);
 
-// Add additional visit functions as needed for handling specific AST node types
-// such as mathematical operations, literals, etc.
+// Additional visit functions for handling specific AST node types
 AST_T* visitor_visit_binary_op(visitor_T* visitor, AST_T* node);
 AST_T* visitor_visit_unary_op(visitor_T* visitor, AST_T* node);
 AST_T* visitor_visit_literal(visitor_T* visitor, AST_T* node);
 
-#endif
+#endif // VISITOR_H
